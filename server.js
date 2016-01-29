cd65b78da15a457b62f7f82948babb42f0943652
akash <akash.track@gmail.com>
2016-01-29 15:39:24 -0500

    updated server.js

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var app = express();              // creating an instance of express lib
 
 
 app.use(express.static(__dirname + '/public'));
-
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.get('/hello', function(req,res){
+    res.send('hello world');
+});
 
 app.listen(port, ipaddress);
 
