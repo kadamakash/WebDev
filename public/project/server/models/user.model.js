746bb9fc6577669ca50177250cc8dd62fd34cf9f
akash <akash.track@gmail.com>
2016-03-25 19:35:13 -0400

    Upated Server side components for User

--- public/project/server/models/user.model.js
+++ public/project/server/models/user.model.js
@@ -1,3 +1,74 @@
 /**
  * Created by akash on 3/25/16.
  */
+"use strict";
+
+moduel.exports = function(){
+    var users = require("../user.mock.json");
+    var api = {
+        findUserByCredentials: findUserByCredentials,
+        findUserByUsername: findUserByUsername,
+        findUserById: findUserById,
+        createUser: createUser,
+        deleteUser: deleteUser,
+        updateUser: updateUser
+    };
+    return api;
+
+    function findUserByCredentials(credentials){
+        for(var i=0; i<users.length; i++){
+            if(users[i].username == credentials.username){
+                if(users[i].password == credentials.password){
+                    return users[i];
+                }
+            }
+        }
+        return null;
+    }
+
+    function findUserByUsername(username){
+        for(var i=0; i<users.length; i++){
+            if(users[i].username == username){
+                return users[i];
+            }
+        }
+        return null;
+    }
+
+    function findUserById(userId){
+        for(var i=0; i<users.length; i++){
+            if(users[i]._id == userId){
+                return users[i];
+            }
+        }
+        return null;
+    }
+
+    function createUser(user){
+        users[users.length] = user;
+        return user;
+    }
+
+    function deleteUser(userId){
+        for(var i=0; i<users.length; i++){
+            if(users[i]._id == userId){
+                users.splice(i, 1);
+            }
+        }
+        return users;
+    }
+
+    function updateUser(userId, user){
+        for(var i=0; i<users.length; i++){
+            if(users[i]._id == userId){
+                users[i].firstName = user.firstName;
+                users[i].lastName = user.lastName;
+                users[i].password = user.password;
+                users[i].roles = user.roles;
+                users[i].username = user.username;
+                users[i].email = user.email;
+            }
+        }
+        return user;
+    }
+};
--- public/project/server/services/user.service.server.js
+++ public/project/server/services/user.service.server.js
@@ -1,3 +1,95 @@
 /**
  * Created by akash on 3/25/16.
  */
+"use strict";
+
+module.exports = function(app, model, uuid){
+    app.get("/api/project/user", getAllUsers);
+    app.get("/api/project/user/:id", getUserById);
+    app.get("/api/project/user?username=username", getUserByUsername);
+    app.get("/api/project/user?username=username&password=password", getUserByCredentials);
+    app.put("/api/project/user/:id", updateUserById);
+    app.post("/api/project/user", createUser);
+    app.delete("/api/project/user/:id", deleteUserById);
+
+    function getAllUsers(req, res){
+        if(req.query.username){
+            if(req.query.password){
+                getUserByCredentials(req, res);
+            }
+            else{
+                getUserByUsername(req, res);
+            }
+        }
+        else{
+            var users = model.findAllUsers();
+            res.json(users);
+        }
+    }
+
+    function getUserById(req, res){
+        var id = req.params.id;
+        console.log(req.params);
+        var user = model.findUserById(id);
+        if(user){
+            res.json(user);
+            return;
+        }
+        res.json({message: "No User found"});
+    }
+
+    function getUserByUsername(req, res){
+        var username = req.query.username;
+        console.log(username);
+        var user = model.findUserByUsername(username);
+        if(user){
+            res.json(user);
+            return;
+        }
+        res.json({message: "No User Found"});
+    }
+
+    function getUserByCredentials(req, res){
+        var username = req.query.username;
+        var password = req.query.passowrd;
+        var credentials = {
+            username: username,
+            password: password
+        };
+
+        var user = model.findUserByCredentials(credentials);
+        if(user){
+            res.json(user);
+            return;
+        }
+        res.json({message: "No User Found"});
+    }
+
+    function updateUserById(req, res){
+        var id = req.params.id;
+        var user = req.body;
+        user = model.updateUser(id, user);
+        if(user){
+            res.json(user);
+            return;
+        }
+        res.json({message: "No User Found"});
+
+    }
+
+    function createUser(res, req){
+        var user = req.body;
+        user._id=uuid.v4();
+        model.createUser(user);
+        res.send(user);
+    }
+
+    function deleteUserById(req, res){
+        var id = req.params.id;
+        if(model.deleteUser(id)){
+            res.send(200);
+            return;
+        }
+        res.json ({message: "No User Found"});
+    }
+};
