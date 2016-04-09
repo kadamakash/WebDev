914dae6266787b7cf61d7707d58b2399dd1890da
akash <akash.track@gmail.com>
2016-04-08 20:19:43 -0400

    updated profile and admin routing in config.js

--- public/assignment/client/config.js
+++ public/assignment/client/config.js
@@ -5,83 +5,104 @@
 (function () {
     angular
         .module("FormBuilderApp")
-        .config(function($routeProvider)
+        .config(function ($routeProvider, $httpProvider) {
+            $routeProvider
+
+                .when("/home", {
+                    templateUrl: "views/home/home.view.html",
+                    resolve: {
+                        loggedin: checkCurrentUser
+                    }
+                })
+                .when("/login", {
+                    templateUrl: "views/users/login.view.html",
+                    controller: "LoginController",
+                    controllerAs: "model"
+                })
+                .when("/register", {
+                    templateUrl: "views/users/register.view.html",
+                    controller: "RegisterController",
+                    controllerAs: "model"
+                })
+                .when("/profile", {
+                    templateUrl: "views/users/profile.view.html",
+                    controller: "ProfileController",
+                    controllerAs: "model",
+                    resolve: {
+                        loggedin: checkLoggedIn
+                    }
+                })
+                .when("/forms", {
+                    templateUrl: "views/forms/forms.view.html",
+                    controller: "FormController",
+                    controllerAs: "model"
+                })
+                .when("/form/:formId/fields", {
+                    templateUrl: "views/forms/fields.view.html",
+                    controller: "FieldController",
+                    controllerAs: "model",
+                    resolve: {
+                        loggedin: checkLoggedIn
+                    }
+                })
+                .when("/admin", {
+                    templateUrl: "client/views/admin/admin.html",
+                    controller: 'AdminController',
+                    resolve: {
+                        loggedin: checkAdmin
+                    }
+                })
+                .otherwise({
+                    redirectTo: "/home"
+                })
+        });
+
+    var checkAdmin = function($q, $timeout, $http, $location, $rootScope){
+        var deferred = $q.defer();
+
+        $http.get('/api/assignment/loggedin').success(function(user){
+            $rootScope.errorMessage = null;
+            if(user !== '0' && user.roles.indexOf('admin') != -1) {
+                $rootScope.currentUser = user;
+                defered.resolve();
+            }
+        });
+        return deferred.promise;
+    };
+
+    var checkCurrentUser = function($q, $timeout, $http, $location, $rootScope)
+    {
+        var deferred = $q.defer();
+
+        $http.get('/api/assignment/loggedin').success(function(user)
         {
-        $routeProvider
-            .when("/", {
-                redirectTo: "/home"
-            })
-            .when("/home", {
-                templateUrl: "views/home/home.view.html",
-            })
-            .when("/login", {
-                templateUrl: "views/users/login.view.html",
-                controller: "LoginController",
-                controllerAs: "model"
-            })
-            .when("/register", {
-                templateUrl: "views/users/register.view.html",
-                controller: "RegisterController",
-                controllerAs: "model"
-            })
-            .when("/profile", {
-                templateUrl: "views/users/profile.view.html",
-                controller: "ProfileController",
-                controllerAs: "model",
-                resolve: {
-                    checkLoggedIn: checkLoggedIn
-                }
-            })
-            .when("/forms", {
-                templateUrl: "views/forms/forms.view.html",
-                controller: "FormController",
-                controllerAs: "model"
-            })
-            .when("/form/:formId/fields", {
-                templateUrl: "views/forms/fields.view.html",
-                controller: "FieldController",
-                controllerAs: "model",
-                resolve: {
-                    getLoggedIn: getLoggedIn
-                }
-            })
-            .when("/admin", {
-                templateUrl: "client/views/admin/admin.html",
-                resolve: {
-                    checkLoggedIn: checkLoggedIn
-                }
-            })
-            .otherwise({
-                redirectTo: "/"
-            })
-    });
+            $rootScope.errorMessage = null;
+            if (user !== '0')
+            {
+                $rootScope.cuurentUser = user;
+            }
+            deferred.resolve();
+        });
+        return deferred.promise;
+    };
 
-    function getLoggedIn(UserService, $q) {
+    var checkLoggedIn = function ($q, $timeout, $http, $rootScope, $location) {
         var deferred = $q.defer();
-        UserService
-            .getCurrentSessionUser()
-            .then(function (response) {
-                var currentUser = response.data;
-                UserService.setCurrentUser(currentUser);
+
+        $http.get('/api/assignment/loggedin').success(function (user) {
+            $rootScope.errorMessage = null;
+            // User is Authenticated
+            if (user !== '0') {
+                $rootScope.currentUser = user;
                 deferred.resolve();
-            });
-        return deferred.promise;
-    }
+            }
 
-    function checkLoggedIn(UserService, $q, $location) {
-        var deferred = $q.defer();
-        UserService
-            .getCurrentSessionUser()
-            .then(function (response){
-                var currentUser = response.data;
-                if(currentUser) {
-                    UserService.setCurrentUser(currentUser);
-                    deferred.resolve();
-                } else {
-                    deferred.reject();
-                    $location.url("/home");
-                }
-            });
+            else {
+                $rootScope.error = 'You need to log in';
+                deferred.reject();
+                $location.url('/');
+            }
+        });
         return deferred.promise;
-    }
+    };
 })();
