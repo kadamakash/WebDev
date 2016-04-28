04da4516b96e0c77fb94c2adc210cb137d378e4d
akash <akash.track@gmail.com>
2016-04-28 14:43:02 -0400

    api

--- public/project/client/services/apartment.service.client.js
+++ public/project/client/services/apartment.service.client.js
@@ -12,7 +12,7 @@
         return api;
 
         function findApartmentsByCity(code,callback) {
-            $http.get("https://api.sandbox.amadeus.com/v1.2/hotels/search-airport?apikey=GAGPBZuaEhR81NA3IIhcEH9xpKHgK27O&location="+code+"&check_in=2016-06-14&check_out=2016-06-16")
+            $http.get("https://api.sandbox.amadeus.com/v1.2/hotels/search-airport?apikey=SECRET_KEY&location="+code+"&check_in=2016-06-14&check_out=2016-06-16")
                 .success(callback);
         }
 
